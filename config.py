3bb3932eb507359f09aeb2bd796b5f11759eebec
ROHIT12-bit <reddyrohit823@gmail.com>
2025-12-23 20:08:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-from os import getenv
+import os
 
 class Config:
-    API_ID = int(getenv("API_ID", "26047636"))  # replace with your default/fallback
-    API_HASH = getenv("API_HASH", "d8b1ed69ae1f937c5dd4d3cc8c8de440")
-    BOT_TOKEN = getenv("BOT_TOKEN", "8549321688:AAHnCSOG7AyFSHnT6VkDWyjFk8Y1cOEXvcw")
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://pokemonchannel098:yaE7BvFwWIXdb3HQ@cluster0.gdr57.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    DB_NAME = getenv("DB_NAME", "AutoApproveDB")
-    OWNER_ID = int(getenv("OWNER_ID", "8367080346"))  # optional, but useful for admin checks
+    API_ID = int(os.getenv("API_ID", "26047636"))
+    API_HASH = os.getenv("API_HASH", "d8b1ed69ae1f937c5dd4d3cc8c8de440")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "8549321688:AAHnCSOG7AyFSHnT6VkDWyjFk8Y1cOEXvcw")
+    MONGO_URI = os.getenv("MONGO_URI")
+    DB_NAME = os.getenv("DB_NAME", "AutoApproveDB")
+    OWNER_ID = int(os.getenv("OWNER_ID", "8367080346"))
     ADMINS = list(map(int, os.getenv("ADMINS", "8367080346").split()))
